d79556a61b6933358476b0ea69f26ba2cbb0e4f7
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2020-02-25 22:58:01 +0200

    Attribute class to provide string representation of enum value

---
+++ WeatherForecast.DarkSkyApi.Library/Attributes/EnumStringValueAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WeatherForecast.DarkSkyApi.Library.Attributes
+{
+    /// <summary>
+    ///     Represents an attribute class to provide string representation of enum value.
+    /// </summary>
+    [AttributeUsage( AttributeTargets.Field )]
+    public class EnumStringValueAttribute : Attribute
+    {
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="EnumStringValueAttribute" /> class.
+        /// </summary>
+        /// <param name="value">A string enum value</param>
+        public EnumStringValueAttribute( string value )
+        {
+            Value = value;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Gets a string value of the enum value.
+        /// </summary>
+        public string Value
+        {
+            get;
+        }
+
+        #endregion
+    }
+}
