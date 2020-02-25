24196d6c48d6909dd1c553411a33482cde452ad6
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2020-02-25 22:19:32 +0200

    [Models] weather alert model

---
+++ WeatherForecast.DarkSkyApi.Library/Models/Alert.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using WeatherForecast.DarkSkyApi.Library.Converters;
+
+namespace WeatherForecast.DarkSkyApi.Library.Models
+{
+    /// <summary>
+    ///     Represents a class that contains a severe weather alert issued for the requested location.
+    /// </summary>
+    public class Alert
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets/Sets a detailed description of the alert.
+        /// </summary>
+        [JsonProperty( "description", Required = Required.Always )]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the UNIX time at which the alert will expire.
+        /// </summary>
+        [JsonProperty( "expires", Required = Required.Always )]
+        [JsonConverter( typeof( UtcDateTimeOffsetJsonConverter ) )]
+        public DateTimeOffset Expires
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets a list of strings representing the names of the regions covered by this weather alert.
+        /// </summary>
+        [JsonProperty( "regions", Required = Required.Always )]
+        public IList<string> Regions
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the severity of the weather alert.
+        /// </summary>
+        [JsonProperty( "severity", Required = Required.Always )]
+        public Severity Severity
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the UNIX time at which the alert was issued.
+        /// </summary>
+        [JsonProperty( "time", Required = Required.Always )]
+        [JsonConverter( typeof( UtcDateTimeOffsetJsonConverter ) )]
+        public DateTimeOffset Time
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets a brief description of the alert.
+        /// </summary>
+        [JsonProperty( "title", Required = Required.Always )]
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets an HTTP(S) URI that one may refer to for detailed information about the alert.
+        /// </summary>
+        [JsonProperty( "uri", Required = Required.Always )]
+        public string Uri
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
---
+++ WeatherForecast.DarkSkyApi.Library/Models/Severity.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace WeatherForecast.DarkSkyApi.Library.Models
+{
+    /// <summary>
+    ///     Represents the severity of a weather alert.
+    /// </summary>
+    [JsonConverter( typeof( StringEnumConverter ) )]
+    public enum Severity
+    {
+        /// <summary>
+        ///     The severity of this alert is not recognized.
+        /// </summary>
+        [EnumMember( Value = null )]
+        Unknown,
+
+        /// <summary>
+        ///     Indicates that individuals should be aware of potentially severe weather.
+        /// </summary>
+        [EnumMember( Value = "advisory" )]
+        Advisory,
+
+        /// <summary>
+        ///     Indicates that individuals should prepare for potentially severe weather.
+        /// </summary>
+        [EnumMember( Value = "watch" )]
+        Watch,
+
+        /// <summary>
+        ///     Indicates that individuals should take immediate action to protect themselves and others from potentially severe weather.
+        /// </summary>
+        [EnumMember( Value = "warning" )]
+        Warning
+    }
+}
