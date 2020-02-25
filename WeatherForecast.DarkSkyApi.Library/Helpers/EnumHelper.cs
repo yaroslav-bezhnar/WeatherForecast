7a3eb9b221a6dad2b4cdbf77e90cacbf935827a9
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2020-02-25 23:14:35 +0200

    enum helper

---
+++ WeatherForecast.DarkSkyApi.Library/Helpers/EnumHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using WeatherForecast.DarkSkyApi.Library.Attributes;
+
+namespace WeatherForecast.DarkSkyApi.Library.Helpers
+{
+    /// <summary>
+    /// Provides helper methods for <see cref="Enum"/> members.
+    /// </summary>
+    public static class EnumHelper
+    {
+        public static TEnum GetDefaultValue<TEnum>() where TEnum : struct
+        {
+            var type = typeof( TEnum );
+            var attributes = (DefaultValueAttribute[]) type.GetCustomAttributes( typeof( DefaultValueAttribute ), false );
+
+            return attributes.Length > 0 ? (TEnum) attributes[0].Value : default( TEnum );
+        }
+
+        public static string JoinEnumParameters<TEnum>( this IList<TEnum> values ) where TEnum : struct
+            => values != null
+                   ? string.Join( ',', values.Cast<Enum>().Select( GetSpecifiedValue ) )
+                   : string.Empty;
+
+        public static string GetSpecifiedValue( this Enum @enum )
+        {
+            var memberInfos = @enum.GetType().GetMember( @enum.ToString() );
+            var attributes = memberInfos[0].GetCustomAttributes( typeof( EnumStringValueAttribute ), false );
+            var specifiedValue = attributes.OfType<EnumStringValueAttribute>().FirstOrDefault()?.Value;
+
+            return specifiedValue ?? @enum.ToString();
+        }
+    }
+}
