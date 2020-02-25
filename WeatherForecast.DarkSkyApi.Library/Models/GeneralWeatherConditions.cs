d92f67a3ae8e818e81a846784edd37e40c06b0a6
Yaroslav Bezhnar <yaroslav.bezhnar@yahoo.com>
2020-02-25 22:30:59 +0200

    [Models] weather forecast model classes

---
+++ WeatherForecast.DarkSkyApi.Library/Models/GeneralWeatherConditions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace WeatherForecast.DarkSkyApi.Library.Models
+{
+    /// <summary>
+    ///     Represents a class that contains the various weather phenomena occurring over a period of time.
+    /// </summary>
+    /// <typeparam name="TCondition">Weather forecast condition.</typeparam>
+    public class GeneralWeatherConditions<TCondition> where TCondition : GeneralForecast
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets/Sets a human-readable summary of the forecast.
+        /// </summary>
+        [JsonProperty( "summary" )]
+        public string Summary
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets a machine-readable text summary that can be used to select an icon to display.
+        /// </summary>
+        [JsonProperty( "icon" )]
+        public string Icon
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the list of <see cref="TCondition" />, ordered by time, which together describe the weather
+        ///     conditions at the requested location over time.
+        /// </summary>
+        [JsonProperty( "data", Required = Required.Always )]
+        public IList<TCondition> Data
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
---
+++ WeatherForecast.DarkSkyApi.Library/Models/WeatherForecastResponse.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace WeatherForecast.DarkSkyApi.Library.Models
+{
+    /// <summary>
+    ///     Represents a class that contains a weather forecast for a particular location.
+    /// </summary>
+    public class WeatherForecastResponse
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets/Sets the requested latitude.
+        /// </summary>
+        [JsonProperty( "latitude", Required = Required.Always )]
+        public float Latitude
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the requested longitude.
+        /// </summary>
+        [JsonProperty( "longitude", Required = Required.Always )]
+        public float Longitude
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the IANA timezone name for the requested location.
+        /// </summary>
+        [JsonProperty( "timezone", Required = Required.Always )]
+        public string TimeZone
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the current timezone offset in hours.
+        ///     Deprecated: use 'TimeZone', instead.
+        /// </summary>
+        [JsonProperty( "offset" )]
+        [Obsolete( "Use 'TimeZone', instead." )]
+        public double Offset
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the the current weather conditions at the requested location.
+        /// </summary>
+        [JsonProperty( "currently" )]
+        public CurrentForecast Currently
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the weather conditions minute-by-minute for the next hour.
+        /// </summary>
+        [JsonProperty( "minutely" )]
+        public GeneralWeatherConditions<MinuteForecast> Minutely
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the weather conditions hour-by-hour for the next two days.
+        /// </summary>
+        [JsonProperty( "hourly" )]
+        public GeneralWeatherConditions<HourForecast> Hourly
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the weather conditions day-by-day for the next week.
+        /// </summary>
+        [JsonProperty( "daily" )]
+        public GeneralWeatherConditions<DayForecast> Daily
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets any severe weather alerts pertinent to the requested location.
+        /// </summary>
+        [JsonProperty( "alerts" )]
+        public IList<Alert> Alerts
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Gets/Sets the metadata (flags) associated with this weather forecast.
+        /// </summary>
+        [JsonProperty( "flags" )]
+        public Flags Flags
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
